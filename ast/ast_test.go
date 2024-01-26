97928241cd240c9f793f8905e495aa22d957d692
GerinAryoPrasetia <gerinaryo14@gmail.com>
2024-01-26 22:34:13 +0700

    add test for String method in AST

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -20,9 +20,16 @@ func TestString(t *testing.T) {
 					Value: "anotherVar",
 				},
 			},
+			&ReturnStatement{
+				Token: token.Token{Type: token.RETURN, Literal: "return"},
+				ReturnValue: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "9"},
+					Value: "9",
+				},
+			},
 		},
 	}
-	if program.String() != "let myVar = anotherVar;" {
+	if program.String() != "let myVar = anotherVar;return 9;" {
 		t.Errorf("program.String() wrong. got=%q", program.String())
 	}
 }
